7c0cd76243ff42364ab77792fa5bbb0fd3356624
sven <Fredrik-98@live.se>
2026-08-02 16:24:32 +0200

    Time: 0 ms (100%), Space: 19.6 MB (16.48%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,18 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        out = []
+
+        for i in range(1, n+ 1):
+            s = ""
+
+            if i % 3 == 0:
+                s+= "Fizz"
+            if i %5 == 0 :
+                s += "Buzz"
+
+            if s == "":
+                s = str(i)
+
+            out.append(s)
+
+        return out
