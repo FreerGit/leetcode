d00765709ce416cc42f85533bf69ffedfeca47d8
sven <Fredrik-98@live.se>
2026-08-03 13:59:36 +0200

    Time: 3 ms (46.89%), Space: 19.4 MB (14.28%) - LeetHub

---
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def alternatingSum(self, nums: List[int]) -> int:
+        tot = 0
+
+        for i in range(len(nums)):
+            if i % 2 == 0:
+                tot += nums[i]
+            else:
+                tot -= nums[i]
+
+        return tot
