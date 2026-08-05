59afac9a914b185baaffdde2aedab8288f51e96f
sven <Fredrik-98@live.se>
2026-08-05 19:27:25 +0200

    Time: 665 ms (5.05%), Space: 19.4 MB (32.58%) - LeetHub

---
+++ 1534-count-good-triplets/1534-count-good-triplets.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+        ans = 0
+
+        for i in range(len(arr)):
+            for j in range(len(arr)):
+                for k in range(len(arr)):
+                    if i < j < k and abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                        ans += 1
+
+        return ans
